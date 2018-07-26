49423660ede58ab359cd48d4749566f708f27f89
iliyasappazov <iliyasappazov@gmail.com>
2018-07-26 17:06:53 +0600

    initial rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from "rollup-plugin-babel";
+import {uglify} from "rollup-plugin-uglify";
+
+export default {
+    input: "lib/apiConnector.js",
+    output: {
+        file: "dist/api-connector.js",
+        format: "cjs",
+        exports: "named",
+        sourceMap: "inline"
+    },
+    plugins: [
+        babel({
+            babelrc: false,
+            presets: [
+                "es2015-rollup"
+            ],
+            plugins: ["transform-object-rest-spread"],
+            comments: false,
+            runtimeHelpers: true
+        }),
+        uglify(),
+    ],
+    external: ["axios"]
+}
